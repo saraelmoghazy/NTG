e93761affda8e83c0e1ba37f30d15e69e9946f71
Sara Elmoghazy <sara.elmoghazy@yahoo.com>
2018-01-24 13:53:13 +0200

    add sample project

---
+++ app/src/main/java/com/ntg/user/mvpsample/MainActivity.java
@@ -0,0 +1,32 @@
+package com.ntg.user.mvpsample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.refresh:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ app/src/main/java/com/ntg/user/mvpsample/network/APIClient.java
@@ -0,0 +1,8 @@
+package com.ntg.user.mvpsample.network;
+
+/**
+ * Created by Sara Elmoghazy on 24/01/2018.
+ */
+
+public class APIClient {
+}
